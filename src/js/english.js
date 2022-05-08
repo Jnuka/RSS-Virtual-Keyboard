58bef5e21678069c5523c7c4190446a46d380312
Jnuka <jnuka@mail.ru>
2022-05-08 23:53:32 +0400

    feat: add two alphabets

---
+++ src/js/english.js
@@ -0,0 +1,281 @@
+export default {
+  Backquote: {
+    letterSmall: '`',
+    letterBig: '~',
+    buttonClass: 'quot',
+  },
+  Digit1: {
+    letterSmall: '1',
+    letterBig: '!',
+  },
+  Digit2: {
+    letterSmall: '2',
+    letterBig: '@',
+  },
+  Digit3: {
+    letterSmall: '3',
+    letterBig: '#',
+  },
+  Digit4: {
+    letterSmall: '4',
+    letterBig: '$',
+  },
+  Digit5: {
+    letterSmall: '5',
+    letterBig: '%',
+  },
+  Digit6: {
+    letterSmall: '6',
+    letterBig: '^',
+  },
+  Digit7: {
+    letterSmall: '7',
+    letterBig: '&',
+  },
+  Digit8: {
+    letterSmall: '8',
+    letterBig: '*',
+  },
+  Digit9: {
+    letterSmall: '9',
+    letterBig: '(',
+  },
+  Digit0: {
+    letterSmall: '0',
+    letterBig: ')',
+  },
+  Minus: {
+    letterSmall: '-',
+    letterBig: '_',
+  },
+  Equal: {
+    letterSmall: '=',
+    letterBig: '+',
+  },
+  Backspace: {
+    letterSmall: 'BackSpace',
+    letterBig: 'BackSpace',
+    buttonClass: 'backspace',
+  },
+  Tab: {
+    letterSmall: 'Tab',
+    letterBig: 'Tab',
+    buttonClass: 'tab',
+  },
+  KeyQ: {
+    letterSmall: 'q',
+    letterBig: 'Q',
+  },
+  KeyW: {
+    letterSmall: 'w',
+    letterBig: 'W',
+  },
+  KeyE: {
+    letterSmall: 'e',
+    letterBig: 'E',
+  },
+  KeyR: {
+    letterSmall: 'r',
+    letterBig: 'R',
+  },
+  KeyT: {
+    letterSmall: 't',
+    letterBig: 'T',
+  },
+  KeyY: {
+    letterSmall: 'y',
+    letterBig: 'Y',
+  },
+  KeyU: {
+    letterSmall: 'u',
+    letterBig: 'U',
+  },
+  KeyI: {
+    letterSmall: 'i',
+    letterBig: 'I',
+  },
+  KeyO: {
+    letterSmall: 'o',
+    letterBig: 'O',
+  },
+  KeyP: {
+    letterSmall: 'p',
+    letterBig: 'P',
+  },
+  BracketLeft: {
+    letterSmall: '[',
+    letterBig: '[',
+  },
+  BracketRight: {
+    letterSmall: ']',
+    letterBig: ']',
+  },
+  Backslash: {
+    letterSmall: '\\',
+    letterBig: '|',
+    buttonClass: 'slash',
+  },
+  Delete: {
+    letterSmall: 'Del',
+    letterBig: 'Del',
+    buttonClass: 'del',
+  },
+  CapsLock: {
+    letterSmall: 'CapsLock',
+    letterBig: 'CapsLock',
+    buttonClass: 'capslock',
+  },
+  KeyA: {
+    letterSmall: 'a',
+    letterBig: 'A',
+  },
+  KeyS: {
+    letterSmall: 's',
+    letterBig: 'S',
+  },
+  KeyD: {
+    letterSmall: 'd',
+    letterBig: 'D',
+  },
+  KeyF: {
+    letterSmall: 'f',
+    letterBig: 'F',
+  },
+  KeyG: {
+    letterSmall: 'g',
+    letterBig: 'G',
+  },
+  KeyH: {
+    letterSmall: 'h',
+    letterBig: 'H',
+  },
+  KeyJ: {
+    letterSmall: 'j',
+    letterBig: 'J',
+  },
+  KeyK: {
+    letterSmall: 'k',
+    letterBig: 'K',
+  },
+  KeyL: {
+    letterSmall: 'l',
+    letterBig: 'L',
+  },
+  Semicolon: {
+    letterSmall: ';',
+    letterBig: '[',
+    buttonClass: 'semi',
+  },
+  Quote: {
+    letterSmall: "'",
+    letterBig: "'",
+    buttonClass: 'quot-single',
+  },
+  Enter: {
+    letterSmall: 'Enter',
+    letterBig: 'Enter',
+    buttonClass: 'enter',
+  },
+  ShiftLeft: {
+    letterSmall: 'Shift',
+    letterBig: 'Shift',
+    buttonClass: 'shift',
+  },
+  KeyZ: {
+    letterSmall: 'z',
+    letterBig: 'Z',
+  },
+  KeyX: {
+    letterSmall: 'x',
+    letterBig: 'X',
+  },
+  KeyC: {
+    letterSmall: 'c',
+    letterBig: 'C',
+  },
+  KeyV: {
+    letterSmall: 'v',
+    letterBig: 'V',
+  },
+  KeyB: {
+    letterSmall: 'b',
+    letterBig: 'B',
+  },
+  KeyN: {
+    letterSmall: 'n',
+    letterBig: 'N',
+  },
+  KeyM: {
+    letterSmall: 'm',
+    letterBig: 'M',
+  },
+  Comma: {
+    letterSmall: ',',
+    letterBig: '<',
+    buttonClass: 'comma',
+  },
+  Period: {
+    letterSmall: '.',
+    letterBig: '>',
+    buttonClass: 'dot',
+  },
+  Slash: {
+    letterSmall: '/',
+    letterBig: '?',
+  },
+  ArrowUp: {
+    letterSmall: '⮙',
+    letterBig: '⮙',
+    buttonClass: 'arow',
+  },
+  ShiftRight: {
+    letterSmall: 'Shift',
+    letterBig: 'Shift',
+    buttonClass: 'shift',
+  },
+  ControlLeft: {
+    letterSmall: 'Ctrl',
+    letterBig: 'Ctrl',
+    buttonClass: 'ctrl',
+  },
+  MetaLeft: {
+    letterSmall: 'Win',
+    letterBig: 'Win',
+    buttonClass: 'win',
+  },
+  AltLeft: {
+    letterSmall: 'Alt',
+    letterBig: 'Alt',
+    buttonClass: 'alt',
+  },
+  Space: {
+    letterSmall: '____',
+    letterBig: '____',
+    buttonClass: 'space',
+  },
+  AltRight: {
+    letterSmall: 'Alt',
+    letterBig: 'Alt',
+    buttonClass: 'alt',
+  },
+  ArrowLeft: {
+    letterSmall: '⮘',
+    letterBig: '⮘',
+    buttonClass: 'arow',
+  },
+  ArrowDown: {
+    letterSmall: '⮛',
+    letterBig: '⮛',
+    buttonClass: 'arow',
+  },
+  ArrowRight: {
+    letterSmall: '⮚',
+    letterBig: '⮚',
+    buttonClass: 'arow',
+  },
+  ControlRight: {
+    letterSmall: 'Ctrl',
+    letterBig: 'Ctrl',
+    buttonClass: 'ctrl',
+  },
+};
